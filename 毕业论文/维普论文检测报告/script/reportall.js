d98eaaca19f3e78c52ca73d361e048e4c034cefb
jtahstu <root@jtahstu.com>
2017-05-22 12:02:41 +0800

    添加维普检测报告

---
+++ 毕业论文/维普论文检测报告/script/reportall.js
@@ -0,0 +1,44 @@
+﻿$(function () {
+		$('#cqvip-jianceorgan-paperreport-highcharts').highcharts({
+				exporting: {
+						enabled: false
+				},
+				chart: {
+						plotBackgroundColor: null,
+						plotBorderWidth: null,
+						plotShadow: false,
+						backgroundColor: '#fefcf1',
+						width: 320,
+						height: 190
+				},
+				colors: ['#32cd32', '#FF0000', '#FFFF66'],
+				title: {
+						text: ''
+				},
+				tooltip: {
+						pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+				},
+				plotOptions: {
+						pie: {
+								allowPointSelect: true,
+								cursor: 'pointer',
+								dataLabels: {
+										enabled: true,
+										color: '#000000',
+										connectorColor: '#000000',
+										format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+								},
+								showInLegend: true
+						}
+				},
+				series: [{
+						type: 'pie',
+						name: '百分比',
+						data: [
+						['自写率', ownpercentage],
+						['复写率', duplicatepercentage],
+						['引用率', quotepercentage]
+				]
+				}]
+		});
+});
---
+++ 毕业论文/维普论文检测报告/script/reportshow.js
@@ -0,0 +1,35 @@
+﻿$(function(){
+	$('.cqvip-jiance-paperreportpart').click(function(){
+		var partid=$(this).attr('partid');
+		$('.partInfo').hide();
+		$('#detail_'+partid).show();
+	});
+	$('.cqvip-jiance-reportdrop').live('click',function(){
+		if(!confirm('您确认进行此操作吗？')){return;}
+		var paperid=$(this).attr('paperid');
+		var partid=$(this).attr('partid');
+		var dropsign=$(this).attr('dropsign');
+		
+		$('.cqvip-jiance-paperreportpart').each(function(i){
+			var curobj=$(this);
+			var curpartid=$(this).attr('partid');
+			if(curpartid == partid){
+				$.ajax({
+					dataType: 'json',
+					url: 'organpartdrop.aspx?paperid=' + paperid + '&partid=' + partid + '&dropsign=' + dropsign,
+					cache: false,
+					success: function(data){
+						if(data.success){
+							curobj.replaceWith(curobj.html());
+							$('#detail_'+partid).hide();
+							$('.cqvip-jiance-percentage').html((Number($('.cqvip-jiance-percentage').text())-data.schedule).toFixed(2));
+							$('.cqvip-jiance-partnum').html(Number($('.cqvip-jiance-partnum').text())-1);
+						}else{
+							alert(data.message);
+						}
+					}
+				});
+			}
+		});
+	});
+});
